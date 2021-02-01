4cdbc0e87709589aa65fbfc0b3006c1db15def66
maanan-universeboss <74287012+maanan-universeboss@users.noreply.github.com>
2021-02-01 14:20:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1,box2,box3,box4,box5;
+var bird;
+var pig1,pig2;
+var log1,log2,log3,log4;
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    ground = new Ground(600,height,1200,20)
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(900,320,70,70);
+    pig1=new Pig(800,320);
+    log1=new Log(800,260,300,PI/2);
+
+    box3=new Box(700,230,70,70);
+    box4=new Box(900,230,70,70);
+    pig2=new Pig(800,240);
+    log2=new Log(800,140,300,PI/2);
+
+    box5=new Box(800,120,70,70);
+    log3=new Log(730,120,150,PI/7);
+    log4=new Log(880,120,150,-PI/7);
+
+    bird=new Bird(100,100);
+
+    
+
+    
+
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    ground.display();
+    box1.display();
+    box2.display();
+   
+    bird.display();
+    pig1.display();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig2.display();
+    log2.display();
+
+    box5.display();
+    log3.display();
+    log4.display();
+}
